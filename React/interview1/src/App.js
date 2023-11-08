85c33f652cf7f12bf929f0a7aa9340d57edb6aed
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-11-08 13:56:48 -0800

    init

---
+++ React/interview1/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("")
+
+  const addTodo = () => {
+    setTodos((previous) => [...previous, todoValue])
+    setTodoValue("")
+  }
+
+  return (
+    <div className="App">
+      <input value={todoValue} onChange={(e) => {
+       setTodoValue(e.target.value)
+      }}></input>
+      <button onClick={addTodo}>Add Todo</button>
+      {todos.map((value, index) => {
+        return <h1>{index + 1}. {value}</h1>
+      })}
+    </div>
+  );
+}
+
+export default App;
